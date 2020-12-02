7ccb52a1586cd210de73062b5a84c81ad73dd001
stormheng <stormheng@gmail.com>
2020-12-03 01:18:04 +0800

    deploy

---
+++ precache-manifest.aa2a8dd7af3a6fe7fd4dd1f54463160c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae74f3d506b806fd424a",
+    "url": "/cashbook/css/app.b9a8df49.css"
+  },
+  {
+    "revision": "d0a2d863cc8e245610a431c80b9a9975",
+    "url": "/cashbook/index.html"
+  },
+  {
+    "revision": "ae74f3d506b806fd424a",
+    "url": "/cashbook/js/app.5e65c957.js"
+  },
+  {
+    "revision": "d233ef3c9b3c2c8263f4",
+    "url": "/cashbook/js/chunk-vendors.ffb21e19.js"
+  },
+  {
+    "revision": "b384eafa891f01b91d307c7a728467fc",
+    "url": "/cashbook/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cashbook/robots.txt"
+  }
+]);
